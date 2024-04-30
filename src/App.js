d94b76c2f5e0d5d2b60d3cd285d6ab585fe12467
hajarbnh33 <hb-79@hotmail.fr>
2024-04-30 02:11:35 +0200

    modif

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <div>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
         </Routes>
       </Router>
-      </div>
   );
 }
 
